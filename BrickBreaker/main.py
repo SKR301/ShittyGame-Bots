48f9a2f3dea5d8a1ae810da2f160dcffcf3f5ca3
SKR301 <chunnu.bihat@gmail.com>
2022-06-07 16:02:17 +0530

    #6 made bot for brick breaker

--- BrickBreaker/main.py
+++ BrickBreaker/main.py
@@ -4,24 +4,14 @@ A python GUI bot to play 'brick breakers'
 
 from pyautogui import *
 import pyautogui
-import time
 import keyboard
-import random
-import win32api, win32con
 
-gameFrame = {"startX":587,"startY":310,"endX":1241,"endY":821}					#setting game frame pixels
-elementPixel = {"r":255,"g":255,"b":255}											# RGB value to find and click
+gameFrame = {"startX":580,"startY":620,"endX":1240,"endY":820}					#setting game frame pixels
 
-while keyboard.is_pressed("q") == False:	
-    pic = pyautogui.screenshot(region=(gameFrame["startX"],gameFrame["startY"],gameFrame["endX"],gameFrame["endY"]))
-    width,height = pic.size
-
-    for x in range (gameFrame["startX"], gameFrame["endX"], 5):					#loop through the whole game frame
-        for y in range (gameFrame["startY"],gameFrame["endY"], 5):
-            # r,g,b = pic.getpixel((x,y))
-            # if r == elementPixel["r"] and b == elementPixel["b"] and g == elementPixel["g"]:
-            #     pixel = {"x":x+gameFrame["startX"],"y":y+gameFrame["startY"]}		#setting pixel to click (adding offset)
-            #     pyautogui.moveTo(pixel["x"], pixel["y"])
-            #     break
-            # print(r,g,b)
-            pyautogui.moveTo(x,y)
+while keyboard.is_pressed("q") == False:
+    try:
+        ballLoc = pyautogui.locateOnScreen('ball.png', region=(gameFrame["startX"],gameFrame["startY"],gameFrame["endX"],gameFrame["endY"]), confidence=0.7)
+        ballX, ballY = pyautogui.center(ballLoc)
+        pyautogui.moveTo(ballX, ballY)
+    except:
+        print()
