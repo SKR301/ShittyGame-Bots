d4e6d4efe3b35f737ed677deff00de3dca8e712b
SKR301 <chunnu.bihat@gmail.com>
2022-06-08 21:09:36 +0530

    added more lookups

--- DinoRun/main.py
+++ DinoRun/main.py
@@ -5,18 +5,26 @@ import pyautogui
 import time
 import keyboard
 
-lookupPixPos = {'x': 734, 'y': 308}
+lookupPixPosBL = {'x': 735, 'y': 310}
+lookupPixPosTL = {'x': 735, 'y': 300}
+lookupPixPosTR = {'x': 745, 'y': 300}
+lookupPixPosBR = {'x': 745, 'y': 310}
+lookupPixPosC = {'x': 740, 'y': 305}
 bgPixPos = {'x': 0, 'y': 500}
 
 while keyboard.is_pressed('q') == False:
-    lookupPixCol = pyautogui.pixel(lookupPixPos['x'], lookupPixPos['y'])
+    lookupPixColBL = pyautogui.pixel(lookupPixPosBL['x'], lookupPixPosBL['y'])
+    lookupPixColTL = pyautogui.pixel(lookupPixPosTL['x'], lookupPixPosTL['y'])
+    lookupPixColTR = pyautogui.pixel(lookupPixPosTR['x'], lookupPixPosTR['y'])
+    lookupPixColBR = pyautogui.pixel(lookupPixPosBR['x'], lookupPixPosBR['y'])
+    lookupPixColC = pyautogui.pixel(lookupPixPosC['x'], lookupPixPosC['y'])
     bgPixCol = pyautogui.pixel(bgPixPos['x'], bgPixPos['y'])
 
-    print(lookupPixCol, bgPixCol)
+    # print(lookupPixCol, bgPixCol)
 
     # jump on cactus
-    if lookupPixCol != bgPixCol:
-        # keyboard.press_and_release('space')
+    if lookupPixPosBL != bgPixCol or lookupPixPosTL != bgPixCol or lookupPixPosTR != bgPixCol or lookupPixPosBR != bgPixCol or lookupPixColC != bgPixCol:
+        keyboard.press_and_release('space')
         print('jump')
 
     # crouch on dragon
