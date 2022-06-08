100ccd39d7c23bc5c5050b4561e6ff8506fd2cb9
SKR301 <chunnu.bihat@gmail.com>
2022-06-08 21:17:33 +0530

    removed excessive lookups

--- DinoRun/main.py
+++ DinoRun/main.py
@@ -5,25 +5,26 @@ import pyautogui
 import time
 import keyboard
 
-lookupPixPosBL = {'x': 735, 'y': 310}
-lookupPixPosTL = {'x': 735, 'y': 300}
-lookupPixPosTR = {'x': 745, 'y': 300}
-lookupPixPosBR = {'x': 745, 'y': 310}
-lookupPixPosC = {'x': 740, 'y': 305}
+lookupPixPosBL = {'x': 740, 'y': 310}
+# lookupPixPosTL = {'x': 735, 'y': 300}
+# lookupPixPosTR = {'x': 745, 'y': 300}
+# lookupPixPosBR = {'x': 745, 'y': 310}
+# lookupPixPosC = {'x': 740, 'y': 305}
 bgPixPos = {'x': 0, 'y': 500}
 
 while keyboard.is_pressed('q') == False:
     lookupPixColBL = pyautogui.pixel(lookupPixPosBL['x'], lookupPixPosBL['y'])
-    lookupPixColTL = pyautogui.pixel(lookupPixPosTL['x'], lookupPixPosTL['y'])
-    lookupPixColTR = pyautogui.pixel(lookupPixPosTR['x'], lookupPixPosTR['y'])
-    lookupPixColBR = pyautogui.pixel(lookupPixPosBR['x'], lookupPixPosBR['y'])
-    lookupPixColC = pyautogui.pixel(lookupPixPosC['x'], lookupPixPosC['y'])
+    # lookupPixColTL = pyautogui.pixel(lookupPixPosTL['x'], lookupPixPosTL['y'])
+    # lookupPixColTR = pyautogui.pixel(lookupPixPosTR['x'], lookupPixPosTR['y'])
+    # lookupPixColBR = pyautogui.pixel(lookupPixPosBR['x'], lookupPixPosBR['y'])
+    # lookupPixColC = pyautogui.pixel(lookupPixPosC['x'], lookupPixPosC['y'])
     bgPixCol = pyautogui.pixel(bgPixPos['x'], bgPixPos['y'])
 
-    # print(lookupPixCol, bgPixCol)
+    # print(lookupPixColBL, lookupPixColTL, lookupPixColTR, lookupPixColBR, lookupPixColC, bgPixCol)
 
     # jump on cactus
-    if lookupPixPosBL != bgPixCol or lookupPixPosTL != bgPixCol or lookupPixPosTR != bgPixCol or lookupPixPosBR != bgPixCol or lookupPixColC != bgPixCol:
+    # if lookupPixColBL != bgPixCol or lookupPixColTL != bgPixCol or lookupPixColTR != bgPixCol or lookupPixColBR != bgPixCol or lookupPixColC != bgPixCol:
+    if lookupPixColBL != bgPixCol:
         keyboard.press_and_release('space')
         print('jump')
 
